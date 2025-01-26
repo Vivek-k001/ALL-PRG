53e9e1d286b4d546796d7044e6389fb93ff0758f
Vivek-k001 <vk3699010@gmail.com>
2025-01-26 12:00:00 +0530

    Forgot to commit on January 26 😅

--- java/Main.java
+++ java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String [] args) {
-        System.out.println("i hate nigggers");
+        System.out.println("i hate nigggerss");
         System.out.println("i hate them");
 
     }
