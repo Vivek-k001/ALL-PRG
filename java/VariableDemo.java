6bb4ec1f2043779531b913e161e5c965c9c480d5
Vivek2cold <vk3699010@gmail.com>
2025-07-09 12:00:00 +0530

    Commit for July 9

---
+++ java/VariableDemo.java
@@ -0,0 +1,15 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        int age = 20;               
+        double price = 99.99;      
+        char grade = 'A';        
+        boolean isPass = true;       
+        String name = "Vivek";        
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPass);
+        System.out.println("Price: ₹" + price);
+    }
+}
