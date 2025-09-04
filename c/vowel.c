d19a100b467d1f505c44b6c9b625274f20684d40
Vivek2cold <vk3699010@gmail.com>
2025-09-04 12:00:00 +0530

    Commit for September 4

--- c/vowel.c
+++ c/vowel.c
@@ -5,7 +5,7 @@ int main() {
     char str[100];
     int i, count = 0;
 
-    printf("Enter a string hrethh: ");
+    printf("Enter a string hretesgehh: ");
     fgets(str, sizeof(str), stdin);
 
     for(i = 0; str[i] != '\0'; i++) {
