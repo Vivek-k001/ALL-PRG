f8d1ea1a5cc036b1c66b44133bc87cb5d927af32
Vivek2cold <vk3699010@gmail.com>
2025-11-20 22:35:15 +0530

    reebrb

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main (String [] args) {
         System.out.println("i hate nigggerss");
-        System.out.println("i hate them");
+        System.out.println("i hate them yess");
 
     }
 }
